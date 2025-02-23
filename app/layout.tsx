9ba235fdad0d72cb32d8270fa496ef1882f003c4
Davi Freire <fsdavi.dev@gmail.com>
2025-02-23 18:40:22 -0300

    chore: add styled components registry

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
+import StyledComponentsRegistry from "./lib/registry";
 
 const openSans = Open_Sans({
   weight: ["400", "500", "600"],
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={openSans.variable}>
         <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          <StyledComponentsRegistry>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          </StyledComponentsRegistry>
         </AppRouterCacheProvider>
       </body>
     </html>
--- app/utils/typographies.ts
+++ app/utils/typographies.ts
@@ -3,7 +3,11 @@ import { css } from 'styled-components'
 export const title = css`
   color: var(--black);
   font-weight: 600;
-  font-size: 40px;
+  font-size: 28px;
+  
+  @media (min-width: 768px) {
+    font-size: 40px;
+  }
 `
 
 export const subtitle = css`
@@ -15,5 +19,9 @@ export const subtitle = css`
 export const body = css`
   color: var(--gray-1);
   font-weight: 400;
-  font-size: 20px;
+  font-size: 16px;
+
+  @media (min-width: 768px) {
+    font-size: 20px;
+  }
 `
